818d743503fe3a039cbede8d17fa5ce5f499b826
vagrant <vagrant@vagrant.vm>
2018-02-25 05:08:30 +0000

    elaborate test cases

--- tests/esproposal_optional_chain.enable/test.js
+++ tests/esproposal_optional_chain.enable/test.js
@@ -1,13 +1,26 @@
 /* @flow */
 
-var foo = {
+
+var foo : {
+  bar?: {
+    baz?: string,
+    qux?: number,
+  },
+  qux?: {
+    quuz?: {}
+  }
+} = {
   bar: {
     baz: "BAZ",
     qux: 123
   }
 };
+var bar : ?Object = foo?.bar;
+var baz : ?string = foo?.bar?.baz;
+var qux : ?number = foo?.qux;
+var quux : ?Object = foo?.qux?.quuz;
 
-var bar = foo?.bar;
-var baz = foo?.bar?.baz;
-var qux = foo?.qux;
-var quux = foo?.qux?.quuz;
+var bar2 : Object = foo?.bar;  //error - possible undefined
+var baz2 : ?number = foo?.bar?.baz //error - mismatch
+// TODO: more cases
+
